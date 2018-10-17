7380dd919cb8273f4e737c965d86598f0a4ff68e
yevgeniy.shemiakin <shemiakin.yevhen@gmail.com>
2018-10-17 20:31:06 +0300

    Добавление вспомогательных классов для групп

--- addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/Tests/Group/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/Tests/Group/GroupCreationTests.java
@@ -7,11 +7,11 @@ public class GroupCreationTests extends GroupTestBase {
 
     @Test
     public void testGroupCreation() {
-        app.gotoGroupPage();
-        app.initGroupCreation();
-        app.fillGroupForm(new GroupData("test4", "test5", "test6"));
-        app.submitGroupCreation();
-        app.returnToGroupPage();
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("test4", "test5", "test6"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
     }
 
 }
