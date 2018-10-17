aec2d7bbb22ec37daa5ed11deb0ae65d582475c2
yevgeniy.shemiakin <shemiakin.yevhen@gmail.com>
2018-10-17 11:30:39 +0300

    Добавлены функции hello и area (с одним и двумя аргументами)

--- sandbox/src/main/java/ua/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/HelloWorld.java
@@ -2,6 +2,26 @@ package ua.stqa.pft.sandbox;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+	    String somebody = "User";
+		hello(somebody);
+
+        double lenght = 8;
+        System.out.println("Площадь квадрата со стороной " + lenght + " = " + area(lenght));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area (a,b));
 	}
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody);
+    }
+
+    public static double area (double l) {
+	    return l * l;
+    }
+
+    public static double area (double a, double b) {
+	    return a * b;
+    }
 }
