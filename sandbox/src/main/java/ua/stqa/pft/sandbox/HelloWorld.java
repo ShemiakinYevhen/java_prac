692512416533e8b62cb5b46a8e1c929899505b34
yevgeniy.shemiakin <shemiakin.yevhen@gmail.com>
2018-10-17 13:22:52 +0300

    Revert "Revert "Создание отдельных классов дял квадрата и прямоугольника и реализация функций в виде методов этих классов""
    This reverts commit 94f37fe38c724b631ca78a0eb2cb62bda3f58d48.

--- sandbox/src/main/java/ua/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/HelloWorld.java
@@ -5,23 +5,15 @@ public class HelloWorld {
 	    String somebody = "User";
 		hello(somebody);
 
-        double lenght = 8;
-        System.out.println("Площадь квадрата со стороной " + lenght + " = " + area(lenght));
+        Square s = new Square(8);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area (a,b));
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody);
     }
-
-    public static double area (double l) {
-	    return l * l;
-    }
-
-    public static double area (double a, double b) {
-	    return a * b;
-    }
 }
+
