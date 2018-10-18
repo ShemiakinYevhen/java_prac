bc4a194f15eaa98103b2e41837c478d9d962cc7b
yevgeniy.shemiakin <shemiakin.yevhen@gmail.com>
2018-10-18 13:43:32 +0300

    Классы GroupTestBase и AddressTestBase обьеденены в один класс TestBase

--- addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/Tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/Tests/TestBase.java
@@ -1,12 +1,13 @@
-package ua.stqa.pft.addressbook.Tests.Address;
+package ua.stqa.pft.addressbook.Tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ua.stqa.pft.addressbook.AppManager.ApplicationManager;
 
-public class AddressTestBase {
+public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod
     public void setUp() throws Exception {
