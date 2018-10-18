0505c35eb4139523a488914c9620c1cf12e9fc72
yevgeniy.shemiakin <shemiakin.yevhen@gmail.com>
2018-10-18 18:20:25 +0300

    Добавлена проверка необходимости перехода между страницами

--- addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/AppManager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/AppManager/NavigationHelper.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase{
-    private WebDriver wd;
 
     public NavigationHelper(WebDriver wd) {
         super(wd);
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1")) && wd.findElement(By.tagName("h1")).getText().equals("Groups") && isElementPresent(By.name("new"))) {
+           return;
+        }
         click(By.linkText("groups"));
     }
 
@@ -23,6 +25,9 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home"));
     }
 }
