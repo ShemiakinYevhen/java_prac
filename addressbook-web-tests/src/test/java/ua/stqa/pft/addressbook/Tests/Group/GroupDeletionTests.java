11be7a2070ef9cf28b64ff496005fd952444eb2d
yevgeniy.shemiakin <shemiakin.yevhen@gmail.com>
2018-10-18 18:38:18 +0300

    Добавлена проверка наличия созданных групп перед удалением и модификацией

--- addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/Tests/Group/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/Tests/Group/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ua.stqa.pft.addressbook.Tests.Group;
 
 import org.testng.annotations.Test;
+import ua.stqa.pft.addressbook.Models.GroupData;
 import ua.stqa.pft.addressbook.Tests.TestBase;
 
 public class GroupDeletionTests extends TestBase {
@@ -8,6 +9,9 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test4", "test5", null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
