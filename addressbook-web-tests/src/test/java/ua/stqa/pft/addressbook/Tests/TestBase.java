0eed55437dadc9dfdda551b07e57e3c1c065728c
yevgeniy.shemiakin <shemiakin.yevhen@gmail.com>
2018-10-23 18:41:49 +0300

    Добавлено логирование процесса тестирования в консоль и во внешний лог-файл

--- addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/Tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/Tests/TestBase.java
@@ -1,14 +1,22 @@
 package ua.stqa.pft.addressbook.Tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ua.stqa.pft.addressbook.AppManager.ApplicationManager;
+import ua.stqa.pft.addressbook.Tests.Group.GroupCreationTests;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
     protected static ApplicationManager app;
 
     static {
@@ -24,9 +32,18 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logTestStart(Method method, Object[] p) {
+        logger.info("Start test " + method.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method method, Object[] p) {
+        logger.info("Stop test " + method.getName() + " with parameters " + Arrays.asList(p));
+    }
 }
