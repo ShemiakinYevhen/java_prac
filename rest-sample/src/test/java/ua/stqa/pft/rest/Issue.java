a69855bb5b9af57f836b7656194c8a7f2f112a75
yevgeniy.shemiakin <shemiakin.yevhen@gmail.com>
2018-10-30 16:08:53 +0200

    Добавлен тест с использованием библиотеки REST-Assured

--- rest-sample/src/test/java/ua/stqa/pft/rest/Issue.java
+++ rest-sample/src/test/java/ua/stqa/pft/rest/Issue.java
@@ -1,5 +1,7 @@
 package ua.stqa.pft.rest;
 
+import java.util.Objects;
+
 public class Issue {
 
     private int id;
@@ -26,6 +28,21 @@ public class Issue {
 
     public String getDescription() {
         return description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Issue issue = (Issue) o;
+        return id == issue.id &&
+                Objects.equals(subject, issue.subject) &&
+                Objects.equals(description, issue.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, subject, description);
     }
 
     public Issue withDescription(String description) {
--- rest-sample/src/test/java/ua/stqa/pft/rest/RestAssuredTests.java
+++ rest-sample/src/test/java/ua/stqa/pft/rest/RestAssuredTests.java
@@ -4,17 +4,24 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
 import org.apache.http.HttpHost;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.Set;
 
-public class RestTests {
+public class RestAssuredTests {
+
+    @BeforeClass
+    public void init() {
+        RestAssured.authentication = RestAssured.basic("28accbe43ea112d9feb328d2c00b3eed", "");
+    }
 
     @Test
     public void testCreateIssue() throws IOException {
@@ -27,22 +34,16 @@ public class RestTests {
     }
 
     private Set<Issue> getIssues() throws IOException {
-        String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json")).returnContent().asString();
+        String json = RestAssured.get("http://demo.bugify.com/api/issues.json").asString();
         JsonElement parsed = new JsonParser().parse(json);
         JsonElement issues = parsed.getAsJsonObject().get("issues");
         return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
     }
 
-    private Executor getExecutor() {
-        return Executor.newInstance().auth(new HttpHost("http://demo.bugify.com/api"), "7e7753d168b94be35fae6aed4a984924", "");
-
-    }
-
     private int createIssue(Issue newIssue) throws IOException {
-        String json = getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json")
-                .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
-                          new BasicNameValuePair("description", newIssue.getDescription())))
-                .returnContent().asString();
+        String json = RestAssured.given().parameter("subject", newIssue.getSubject())
+                .parameter("description", newIssue.getDescription())
+                .post("http://demo.bugify.com/api/issues.json").asString();
         JsonElement parsed = new JsonParser().parse(json);
         return parsed.getAsJsonObject().get("issue_id").getAsInt();
     }
