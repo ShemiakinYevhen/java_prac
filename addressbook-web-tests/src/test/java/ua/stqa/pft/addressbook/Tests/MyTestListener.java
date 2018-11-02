76a581efdfcd80680fc4a48fc1cbbdae5ab8be52
yevgeniy.shemiakin <shemiakin.yevhen@gmail.com>
2018-11-02 13:44:56 +0200

    Добавлена возможность прикрепления скришнотов в отчеты Allure при запуске через Jenkins

--- addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/Tests/MyTestListener.java
+++ addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/Tests/MyTestListener.java
@@ -1,8 +1,8 @@
 package ua.stqa.pft.addressbook.Tests;
 
+import io.qameta.allure.Attachment;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-import ru.yandex.qatools.allure.annotations.Attachment;
 import ua.stqa.pft.addressbook.AppManager.ApplicationManager;
 
 public class MyTestListener implements ITestListener {
@@ -11,7 +11,11 @@ public class MyTestListener implements ITestListener {
     public void onTestFailure(ITestResult result) {
         ApplicationManager app = (ApplicationManager) result.getTestContext().getAttribute("app");
         saveScreenshot(app.takeScreenshot());
+    }
 
+    public void onTestSuccess(ITestResult result) {
+        ApplicationManager app = (ApplicationManager) result.getTestContext().getAttribute("app");
+        saveScreenshot(app.takeScreenshot());
     }
 
     @Attachment(value = "Page screenshot", type = "image/png")
